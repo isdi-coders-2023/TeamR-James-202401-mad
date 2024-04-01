3b9cb3d1c030496e31f63df68899d840cb951970
raulgonzalez <raulgonji@gmail.com>
2024-04-01 19:39:03 +0200

    Add nav bar

---
+++ src/app/main/navbar/navbar.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zld-navbar',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="navbar">
+      <img src="../assets/burger.svg" alt="logoBurger" />
+      <nav>
+        <ul>
+          <li>
+            <a href=""
+              ><img src="../assets/favorites.svg" alt="logoFavorites"
+            /></a>
+          </li>
+          <li>
+            <a href=""
+              ><img src="../assets/monsters.svg" alt="logoMonsters"
+            /></a>
+          </li>
+          <li>
+            <a href=""
+              ><img src="../assets/treasures.svg" alt="logoTreasures"
+            /></a>
+          </li>
+          <li>
+            <a href=""
+              ><img src="../assets/creatures.svg" alt="logoCreatures"
+            /></a>
+          </li>
+          <li>
+            <a href=""
+              ><img src="../assets/equipment.svg" alt="logoEquipament"
+            /></a>
+          </li>
+          <li>
+            <a href=""
+              ><img src="../assets/materials.svg" alt="logoMaterials"
+            /></a>
+          </li>
+          <li>
+            <a href=""><img src="../assets/create.svg" alt="logoCreate" /></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  `,
+  styles: ``,
+})
+export class NavbarComponent {}
