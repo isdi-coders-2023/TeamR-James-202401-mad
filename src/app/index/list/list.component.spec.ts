043547573cd7166d790cceca9f972d82714c11a8
malethik <quintiliani.d@gmail.com>
2024-04-03 16:50:29 +0200

    updating files fixed

--- src/app/index/list/list.component.spec.ts
+++ src/app/index/list/list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListComponent } from './list.component';
+import  ListComponent  from './list.component';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -11,7 +11,7 @@ describe('ListComponent', () => {
       imports: [ListComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
