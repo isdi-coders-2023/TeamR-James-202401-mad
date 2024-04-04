f4d743d9a49d0423fa00b079b506cac5d83672a7
marcos parajua <marcosparajua@gmail.com>
2024-04-04 09:56:16 +0200

    remove commented code

--- src/app/core/state.service.ts
+++ src/app/core/state.service.ts
@@ -2,26 +2,6 @@ import { Injectable } from '@angular/core';
 import { ItemsData } from './zelda-items-data';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-// const initialState: ItemsData[] = [
-//   {
-//     category: '',
-//     common_locations: [],
-//     description: '',
-//     dlc: false,
-//     drops: [],
-//     id: 0,
-//     image: '',
-//     name: '',
-//     cooking_effect: '',
-//     hearts_recovered: 0,
-//     properties: {
-//       attack: 0,
-//       defense: 0,
-//     },
-//     edible: false,
-//   },
-// ];
-
 @Injectable({
   providedIn: 'root',
 })
