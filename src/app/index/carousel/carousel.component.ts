522df981de3b362dc5d4ce3d698b7b966927156e
malethik <quintiliani.d@gmail.com>
2024-04-08 09:11:36 +0200

    added casousel

---
+++ src/app/index/carousel/carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zld-carousel',
+  standalone: true,
+  imports: [],
+  template: ` <p>carousel works!</p> `,
+  styles: ``,
+})
+export class CarouselComponent {}
